107e5030f602b1500dfbd357e39da8b8fcb5e4c9
José Carlos García <carlos.garcia@knotion.com>
2019-06-29 21:12:43 -0500

    NthFibonacci
    NthFibonacci

---
+++ Easy/NthFibonacci.playground/Contents.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+/*
+func fibonacci(_ n: Int) -> Int {
+    if n == 0 || n == 1 {
+        return n
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+// Simple recursion
+// O(2^n) time | O(n) space
+func getNthFib(_ n: Int) -> Int {
+    if n == 2 {
+        return 1
+    } else if n == 1 {
+        return 0
+    } else {
+        return getNthFib(n - 1) + getNthFib(n - 2)
+    }
+}
+
+// Recursion + Memoization
+// O(n) time | O(n) space
+func getNthFib(_ n: Int, memoize: inout [Int: Int] = [1: 0, 2: 1]) -> Int {
+    if let n = memoize[n] {
+        return n
+    } else {
+        memoize[n] = getNthFib(n - 1, memoize: &memoize) + getNthFib(n - 2, memoize: &memoize)
+        return memoize[n]!
+    }
+}
+*/
+// Iterative -> Optimal
+// Time = O(n)
+// Space = O(1) -> We are not storing anything
+func getNthFib(_ n: Int) -> Int {
+    var lastTwo = [0, 1]
+    var counter = 3
+    while counter <= n {
+        let nextFib = lastTwo[0] + lastTwo[1]
+        lastTwo[0] = lastTwo[1]
+        lastTwo[1] = nextFib
+        counter += 1
+    }
+    return n > 1 ? lastTwo[1] : lastTwo[0]
+}
+
+getNthFib(6)
